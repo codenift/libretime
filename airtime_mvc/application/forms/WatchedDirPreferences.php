f3218ce5a17decc431de99afe9517b0fd00193ad
Keoni Mahelona <keoni@kmahelona.net>
2020-07-12 11:39:47 +1200

    Make field rquired

--- airtime_mvc/application/forms/WatchedDirPreferences.php
+++ airtime_mvc/application/forms/WatchedDirPreferences.php
@@ -42,6 +42,7 @@ class Application_Form_WatchedDirPreferences extends Zend_Form_SubForm
         $track_type = new Zend_Form_Element_Select('track_type');
         $track_type->class = 'input_text';
         $track_type->setLabel(_('Track Type:'));
+        $track_type->setRequired(true);
         $track_type->setMultiOptions($track_type_options);
         $this->addElement($track_type);
     }
